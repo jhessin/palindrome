1b90636ae6961fe2960ada90055f9e0f26e231ca
Jim Hessin <jhessin@gmail.com>
2022-08-10 09:12:18 -0600

    Simple palindrome program

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check for palindrome.");
+            String input = Console.ReadLine();
+            int value = int.Parse(input);
+            Console.WriteLine("You entered " + value);
+            if (checkNum(value))
+            {
+              Console.WriteLine("That is a palindrome!");
+            }
+            else
+            {
+              Console.WriteLine("That is NOT a palindrome");
+            }
+
+            do {
+              value += 1;
+            } while (!checkNum(value));
+
+            Console.WriteLine("Your next Palindrome will occur at " + value);
+        }
+
+        static bool checkNum(int num)
+        {
+          String value = num.ToString();
+          char[] charArray = value.ToCharArray();
+          Array.Reverse(charArray);
+          String reversed = new String(charArray);
+          return value == reversed;
+        }
+    }
+}
